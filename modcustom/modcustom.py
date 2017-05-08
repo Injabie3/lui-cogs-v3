a191cbc38a4efe587c43e99b7c3bc4d17d5c7ea3
Ashton Tito <sedruk@users.noreply.github.com>
2017-05-08 12:54:30 -0700

    [ModCustom] Rename things to avoid conflicts
    * rename things to avoid conflicts w/ red's mod cog

--- modcustom/modcustom.py
+++ modcustom/modcustom.py
@@ -19,12 +19,12 @@ def check_folders():
 
 
 def check_files():
-    blacklist_list = {"users": [], "roles": []}
-    whitelist_list = {"users": [], "roles": []}
+    plonked_perms = {"users": [], "roles": []}
+    override_perms = {"users": [], "roles": []}
 
     files = {
-        "blacklist.json"      : blacklist_list,
-        "whitelist.json"      : whitelist_list
+        "plonked_perms.json"      : plonked_perms,
+        "override_perms.json"      : override_perms
     }
 
     for filename, value in files.items():
@@ -38,11 +38,11 @@ class ModCustom(object):
     
     def __init__(self, bot):
         self.bot = bot
-        self.whitelist_list = dataIO.load_json("data/modcustom/whitelist.json")
-        self.blacklist_list = dataIO.load_json("data/modcustom/blacklist.json")
+        self.override_perms = dataIO.load_json("data/modcustom/override_perms.json")
+        self.plonked_perms = dataIO.load_json("data/modcustom/plonked_perms.json")
         
     def is_plonked(self, server, member): # note: message.server isnt needed
-        if len([x for x in self.blacklist_list["users"] if member.id == x ]) == 0:
+        if len([x for x in self.plonked_perms["users"] if member.id == x ]) == 0:
             return False
         else:
             return True
@@ -51,8 +51,8 @@ class ModCustom(object):
         perms = []
         overrides = [] # these roles have higher precedence than blacklist roles
         for role in member.roles:
-            perms += [x for x in self.blacklist_list["roles"] if x in role.name]
-            overrides += [x for x in self.whitelist_list["roles"] if x in role.name]
+            perms += [x for x in self.plonked_perms["roles"] if x in role.name]
+            overrides += [x for x in self.override_perms["roles"] if x in role.name]
             
         if len(perms) == 0: # no blacklisted roles, we good to go
             return True
@@ -63,118 +63,118 @@ class ModCustom(object):
             
     @commands.group(pass_context=True)
     @checks.is_owner_or_permissions(administrator=True)
-    async def m_blacklist(self, ctx):
+    async def plonked(self, ctx):
         """Bans user from using the bot"""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
 
-    @m_blacklist.command(name="adduser")
+    @plonked.command(name="adduser")
     async def _blacklist_adduser(self, user: discord.Member):
         """Adds user to bot's blacklist"""
-        if user.id not in self.blacklist_list["users"]:
-            self.blacklist_list["users"].append(user.id)
-            dataIO.save_json("data/modcustom/blacklist.json", self.blacklist_list)
+        if user.id not in self.plonked_perms["users"]:
+            self.plonked_perms["users"].append(user.id)
+            dataIO.save_json("data/modcustom/plonked_perms.json", self.plonked_perms)
             await self.bot.say("User has been added to blacklist.")
         else:
             await self.bot.say("User is already blacklisted.")
 
-    @m_blacklist.command(name="addrole")
+    @plonked.command(name="addrole")
     async def _blacklist_addrole(self, role: str):
         """Adds role to bot's blacklist"""
-        if role not in self.blacklist_list["roles"]:
-            self.blacklist_list["roles"].append(role)
-            dataIO.save_json("data/modcustom/blacklist.json", self.blacklist_list)
+        if role not in self.plonked_perms["roles"]:
+            self.plonked_perms["roles"].append(role)
+            dataIO.save_json("data/modcustom/plonked_perms.json", self.plonked_perms)
             await self.bot.say("Role has been added to blacklist.")
         else:
             await self.bot.say("Role is already blacklisted.")
-            
-    @m_blacklist.command(name="removeuser")
+
+    @plonked.command(name="removeuser")
     async def _blacklist_removeuser(self, user: discord.Member):
         """Removes user from bot's blacklist"""
-        if user.id in self.blacklist_list["users"]:
-            self.blacklist_list["users"].remove(user.id)
-            dataIO.save_json("data/modcustom/blacklist.json", self.blacklist_list)
+        if user.id in self.plonked_perms["users"]:
+            self.plonked_perms["users"].remove(user.id)
+            dataIO.save_json("data/modcustom/plonked_perms.json", self.plonked_perms)
             await self.bot.say("User has been removed from blacklist.")
         else:
             await self.bot.say("User is not in blacklist.")
 
-    @m_blacklist.command(name="removerole")
+    @plonked.command(name="removerole")
     async def _blacklist_removerole(self, role: str):
         """Removes role to bot's blacklist"""
-        if role in self.blacklist_list["roles"]:
-            self.blacklist_list["roles"].remove(role)
-            dataIO.save_json("data/modcustom/blacklist.json", self.blacklist_list)
+        if role in self.plonked_perms["roles"]:
+            self.plonked_perms["roles"].remove(role)
+            dataIO.save_json("data/modcustom/plonked_perms.json", self.plonked_perms)
             await self.bot.say("Role has been removed from blacklist.")
         else:
             await self.bot.say("Role is not in blacklist.")
-            
-    @m_blacklist.command(name="clear")
+
+    @plonked.command(name="clear")
     async def _blacklist_clear(self):
         """Clears the blacklist"""
-        self.blacklist_list = {"users": [], "roles": []}
-        dataIO.save_json("data/modcustom/blacklist.json", self.blacklist_list)
+        self.plonked_perms = {"users": [], "roles": []}
+        dataIO.save_json("data/modcustom/plonked_perms.json", self.plonked_perms)
         await self.bot.say("Blacklist is now empty.")
 
     @commands.group(pass_context=True)
     @checks.is_owner_or_permissions(administrator=True)
-    async def m_whitelist(self, ctx):
+    async def overridden(self, ctx):
         """Users who will be able to use the bot"""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
 
-    @m_whitelist.command(name="adduser")
+    @overridden.command(name="adduser")
     async def _whitelist_adduser(self, user: discord.Member):
         """Adds user to bot's whitelist"""
-        if user.id not in self.whitelist_list["users"]:
-            if not self.whitelist_list["users"]:
+        if user.id not in self.override_perms["users"]:
+            if not self.override_perms["users"]:
                 msg = "\nAll users not in whitelist will be ignored (owner, admins and mods excluded)"
             else:
                 msg = ""
-            self.whitelist_list["users"].append(user.id)
-            dataIO.save_json("data/modcustom/whitelist.json", self.whitelist_list)
+            self.override_perms["users"].append(user.id)
+            dataIO.save_json("data/modcustom/override_perms.json", self.override_perms)
             await self.bot.say("User has been added to whitelist." + msg)
         else:
             await self.bot.say("User is already whitelisted.")
 
-    @m_whitelist.command(name="addrole")
+    @overridden.command(name="addrole")
     async def _whitelist_addrole(self, role: str):
         """Adds role to bot's whitelist"""
-        if role not in self.whitelist_list["roles"]:
-            if not self.whitelist_list["roles"]:
+        if role not in self.override_perms["roles"]:
+            if not self.override_perms["roles"]:
                 msg = "\nAll roles not in whitelist will be ignored (owner, admins and mods excluded)"
             else:
                 msg = ""
-            self.whitelist_list["roles"].append(role)
-            dataIO.save_json("data/modcustom/whitelist.json", self.whitelist_list)
+            self.override_perms["roles"].append(role)
+            dataIO.save_json("data/modcustom/override_perms.json", self.override_perms)
             await self.bot.say("Role has been added to whitelist." + msg)
         else:
             await self.bot.say("Role is already whitelisted.")
 
-    @m_whitelist.command(name="removeuser")
+    @overridden.command(name="removeuser")
     async def _whitelist_removeuser(self, user: discord.Member):
         """Removes user from bot's whitelist"""
-        if user.id in self.whitelist_list["users"]:
-            self.whitelist_list["users"].remove(user.id)
-            dataIO.save_json("data/modcustom/whitelist.json", self.whitelist_list)
+        if user.id in self.override_perms["users"]:
+            self.override_perms["users"].remove(user.id)
+            dataIO.save_json("data/modcustom/override_perms.json", self.override_perms)
             await self.bot.say("User has been removed from whitelist.")
         else:
             await self.bot.say("User is not in whitelist.")
 
-    @m_whitelist.command(name="removerole")
+    @overridden.command(name="removerole")
     async def _whitelist_removerole(self, role: str):
         """Adds role to bot's whitelist"""
-        if role in self.whitelist_list["roles"]:
-            self.whitelist_list["roles"].remove(role)
-            dataIO.save_json("data/modcustom/whitelist.json", self.whitelist_list)
+        if role in self.override_perms["roles"]:
+            self.override_perms["roles"].remove(role)
+            dataIO.save_json("data/modcustom/override_perms.json", self.override_perms)
             await self.bot.say("Role has been removed from whitelist.")
         else:
             await self.bot.say("Role is not in whitelist.")
-    
-    @m_whitelist.command(name="clear")
+
+    @overridden.command(name="clear")
     async def _whitelist_clear(self):
         """Clears the whitelist"""
-        self.whitelist_list = {"users": [], "roles": []}
-        dataIO.save_json("data/modcustom/whitelist.json", self.whitelist_list)
+        self.override_perms = {"users": [], "roles": []}
+        dataIO.save_json("data/modcustom/override_perms.json", self.override_perms)
         await self.bot.say("Whitelist is now empty.")
         
 def setup(bot):
