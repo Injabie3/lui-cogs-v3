96b591875ccbbb25318b2a9477ae736217451d19
Injabie3 <injabie3@gmail.com>
2018-11-10 17:19:08 -0800

    [RoleAssigner] Don't assign role if user already has one from list

--- role_assigner/role_assigner.py
+++ role_assigner/role_assigner.py
@@ -111,7 +111,10 @@ class RoleAssigner:
             roles.append(roleObject)
 
         for index, user in enumerate(users):
-            await self.bot.add_roles(user, roles[index % numberOfRoles])
+            anyRoles = [ i for i in user.roles if i in roles ]
+            if not anyRoles:
+                await self.bot.add_roles(user, roles[index % numberOfRoles])
+
         msg = ":white_check_mark: **Role Assigner - Assign:** Roles assigned"
         if role:
             msg += " to users with the {} role.".format(role.name)
