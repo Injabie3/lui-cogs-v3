af4fdf7ade2bb4f7381e6d8b6bbe9b782bada94e
Injabie3 <injabie3@gmail.com>
2019-01-11 22:16:22 -0800

    [Triggered] Clean up code - Remove unnecessary imports. - Force 512px by 512px avatar to have "nicer" image.  If avatar is smaller (e.g. 64x64), it still works as expected. - Revise docstrings. - Remove static assignments and refactor into a loop. - Add typing indicator.

--- triggered/triggered.py
+++ triggered/triggered.py
@@ -3,10 +3,9 @@
 """
 
 import os
-import discord
 import urllib.request
+import discord
 from discord.ext import commands
-from cogs.utils.dataIO import dataIO
 from PIL import Image, ImageChops, ImageOps
 
 SAVE_FOLDER = "data/lui-cogs/triggered/" # Path to save folder.
@@ -19,29 +18,30 @@ def checkFolder():
         print("Creating " + SAVE_FOLDER + " folder...")
         os.makedirs(SAVE_FOLDER)
 
-class Triggered: # pylint: disable=too-many-instance-attributes
+class Triggered: # pylint: disable=too-few-public-methods
     """We triggered, fam."""
-
 
     # Class constructor
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command(name="triggered", pass_context=True)
-    async def triggered(self, ctx):
+    async def triggered(self, ctx, user: discord.Member = None):
         """Are you triggered? Say no more."""
-        savePath = await self._createTrigger(ctx.message.author)
+        if not user:
+            user = ctx.message.author
+        await self.bot.send_typing(ctx.message.channel)
+        savePath = await self._createTrigger(user)
         if not savePath:
             return
-        await self.bot.send_file(ctx.message.channel, savePath),
-
+        await self.bot.send_file(ctx.message.channel, savePath)
 
     async def _createTrigger(self, user):
         """Fetches the user's avatar, and creates a triggered GIF
 
         Parameters:
         -----------
-        user: discord.User
+        user: discord.Member
 
         Returns:
         --------
@@ -51,31 +51,34 @@ class Triggered: # pylint: disable=too-many-instance-attributes
         savePath = "{}{}-trig.gif".format(SAVE_FOLDER, user.id)
         try:
             opener = urllib.request.build_opener()
+            # We need a custom header or else we get a HTTP 403 Unauthorized
             opener.addheaders = [("User-agent", "Mozilla/5.0")]
             urllib.request.install_opener(opener)
-            urllib.request.urlretrieve(user.avatar_url, path)
-        except Exception as error:
-            print(user.avatar_url)
-            print(error)
+            url = "https://cdn.discordapp.com/avatars/{0.id}/{0.avatar}.png?size=512"
+            urllib.request.urlretrieve(url.format(user), path)
+        except urllib.request.ContentTooShortError:
+            return None
+
         avatar = Image.open(path)
+
         if not avatar:
             return
-        OFFSET = 30
-        topRight = ImageChops.offset(avatar, -OFFSET, OFFSET)
-        topLeft = ImageChops.offset(avatar, -OFFSET, -OFFSET+6)
-        botRight = ImageChops.offset(avatar, OFFSET-5, OFFSET-15)
-        botLeft = ImageChops.offset(avatar, -OFFSET+15, OFFSET-10)
 
-        topRight = ImageOps.crop(topRight, OFFSET)
-        botRight = ImageOps.crop(botRight, OFFSET)
-        avatar = ImageOps.crop(avatar, OFFSET)
-        topLeft = ImageOps.crop(topLeft, OFFSET)
-        botLeft = ImageOps.crop(botLeft, OFFSET)
-        topRight.save(savePath, format="GIF",
-                      append_images=[botLeft, botRight, topLeft, avatar],
-                      save_all=True,
-                      duration=30,
-                      loop=0)
+        offsets = [(15, 15), (5, 10), (-15, -15),
+                   (10, -10), (10, 0), (-15, 10),
+                   (10, -5)]
+        images = []
+
+        for xcoord, ycoord in offsets:
+            image = ImageChops.offset(avatar, xcoord, ycoord)
+            image = ImageOps.crop(image, 15)
+            images.append(image)
+        avatar = ImageOps.crop(avatar, 15)
+        avatar.save(savePath, format="GIF",
+                    append_images=images,
+                    save_all=True,
+                    duration=25,
+                    loop=0)
         return savePath
 
 
