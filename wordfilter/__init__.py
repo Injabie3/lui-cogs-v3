bb8bb15f9126e9b2cd0bfe6356b96d4caf43a6fc
Injabie3 <injabie3@gmail.com>
2019-02-22 23:54:33 -0800

    [WordFilter] Add folder creation for logs

--- wordfilter/__init__.py
+++ wordfilter/__init__.py
@@ -5,21 +5,30 @@ deleting a message.
 
 This cog requires paginator.py, obtainable from Rapptz/RoboDanny.
 """
+
+LOG_FOLDER = "log/lui-cogs/wordfilter/"
+
 import logging
+import os
 
 from redbot.core.bot import Red
 from .wordfilter import WordFilter
 
+def checkFolders():
+    """Make sure folder for logs is available."""
+    if not os.path.exists(LOG_FOLDER):
+        os.makedirs(LOG_FOLDER)
 
 def setup(bot: Red):
     """Add the cog to the bot."""
     global LOGGER # pylint: disable=global-statement
+    checkFolders()
     wordFilterCog = WordFilter(bot)
     wordFilterCog.logger = logging.getLogger("red.WordFilter")
     if wordFilterCog.logger.level == 0:
         # Prevents the LOGGER from being loaded again in case of module reload.
         wordFilterCog.logger.setLevel(logging.INFO)
-        handler = logging.FileHandler(filename="data/lui-cogs/wordfilter/info.log",
+        handler = logging.FileHandler(filename="{}info.log".format(LOG_FOLDER),
                                       encoding="utf-8",
                                       mode="a")
         handler.setFormatter(logging.Formatter("%(asctime)s %(message)s",
