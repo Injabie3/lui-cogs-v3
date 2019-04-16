902e2f2bce547e63fe8d4999942f0c0d3df93317
Injabie3 <injabie3@gmail.com>
2019-04-15 23:05:24 -0700

    [LastActive] Add some helper methods, more listeners

--- last_active/last_active.py
+++ last_active/last_active.py
@@ -9,6 +9,11 @@ import os
 from pprint import pprint
 import discord
 from discord.ext import commands
+from .utils import config
+
+KEY_CH_SPECIFIC = "channelSpecific"
+KEY_SERVER_SPECIFIC = "serverSpecific"
+KEY_GLOBAL = "global"
 
 LOGGER = None
 
@@ -27,26 +32,167 @@ class LastActive: # pylint: disable=too-few-public-methods
     # Class constructor
     def __init__(self, bot):
         self.bot = bot
-        self.messages = {}
+        self.config = config.Config("settings.json",
+                                    cogname="lui-cogs/last_active")
+        self.chSpecific = self.config.get(KEY_CH_SPECIFIC, {})
+        self.serverSpecific = self.config.get(KEY_SERVER_SPECIFIC, {})
+        self.userGlobal = self.config.get(KEY_GLOBAL, {})
         self.lock = Lock()
 
-    @commands.command(name="lastactive", pass_context=True)
-    async def lastactive(self, ctx):
+    @commands.group(name="lastactive", pass_context=True)
+    async def lastActive(self, ctx):
         """Last active."""
-        pprint(self.messages)
+        pprint(self.chSpecific)
 
-    async def listener(self, msg):
-        """Listener for messages"""
+    @lastActive.command(name="debug", pass_context=False)
+    async def lastActiveDebug(self):
+        """Turn on debugging."""
+        LOGGER.setLevel(logging.DEBUG)
+        await self.bot.say("Debug mode set.")
+
+    ##################
+    # Public Methods #
+    ##################
+    async def checkLastActive(self, uid, sid=None, cid=None):
+        """Check to see when a user was last active in a specific channel and server.
+
+        Parameters:
+        -----------
+        uid: int
+            The ID of the user you are interested in.
+        sid: int (optional)
+            The ID of the server in which you are interested in.
+        cid: int (optional)
+            The ID of the channel you are interested in. Requires sid to be present.
+
+        Returns:
+        --------
+        lastActive: datetime.datetime or None
+            A datetime object of when the user was last active, or None if the user has
+            not spoken or reacted.
+
+        Raises:
+        -------
+        AttributeError
+            When parameters are missing.
+
+        """
+        if cid:
+            if not sid:
+                raise AttributeError
+            try:
+                return self.chSpecific[sid][cid][uid]
+            except KeyError:
+                return None
+        elif sid:
+            try:
+                return self.serverSpecific[sid][uid]
+            except KeyError:
+                return None
+        else: # uid
+            try:
+                return self.userGlobal[uid]
+            except KeyError:
+                return None
+
+    #############
+    # Listeners #
+    #############
+    async def listenMessage(self, msg):
+        """Listener for messages.
+
+        Parameters:
+        -----------
+        msg: discord.Message
+            The message that triggered this listener.
+        """
         with self.lock:
             sid = msg.server.id
             cid = msg.channel.id
             uid = msg.author.id
-            if sid not in self.messages.keys():
-                self.messages[sid] = {}
-            if cid not in self.messages[sid].keys():
-                self.messages[sid][cid] = {}
-            if uid not in self.messages[sid][cid].keys():
-                self.messages[sid][cid][uid] = datetime.now()
+            LOGGER.debug("Message: sid %s, cid %s, uid %s", sid, cid, uid)
+            self._updateChannelSpecific(uid, sid, cid)
+            self._updateServerSpecific(uid, sid)
+            self._updateGlobal(uid)
+
+    async def listenReaction(self, reaction, user):
+        """Listener for reactions.
+
+        Parameters:
+        -----------
+        reaction: discord.Reaction
+            The reaction.
+        user: discord.Member
+            The user that reacted.
+        """
+        with self.lock:
+            sid = reaction.message.server.id
+            cid = reaction.message.channel.id
+            uid = user.id
+            LOGGER.debug("Reaction: sid %s, cid %s, uid %s", sid, cid, uid)
+            self._updateChannelSpecific(uid, sid, cid)
+            self._updateServerSpecific(uid, sid)
+            self._updateGlobal(uid)
+
+    def _updateChannelSpecific(self, uid, sid, cid):
+        """Update the last active for a specific user on a specific channel.
+
+        Parameters:
+        -----------
+        uid: int
+            The ID of the user you want to update.
+        sid: int
+            The ID of the server you want to update.
+        cid: int
+            The ID of the channel you want to update..
+
+        Returns:
+        --------
+        None, but updates self.chSpecific.
+        """
+        if sid not in self.chSpecific.keys():
+            self.chSpecific[sid] = {}
+        if cid not in self.chSpecific[sid].keys():
+            self.chSpecific[sid][cid] = {}
+
+        self.chSpecific[sid][cid][uid] = datetime.now()
+
+    def _updateServerSpecific(self, uid, sid):
+        """Update the last active for a specific user on a specific server.
+
+        Parameters:
+        -----------
+        uid: int
+            The ID of the user you want to update.
+        sid: int
+            The ID of the server you want to update.
+
+        Returns:
+        --------
+        None, but updates self.serverSpecific.
+        """
+        if sid not in self.serverSpecific.keys():
+            self.serverSpecific[sid] = {}
+
+        self.serverSpecific[sid][uid] = datetime.now()
+
+    def _updateGlobal(self, uid):
+        """Update the last active for a specific user on ANY server.
+
+        Parameters:
+        -----------
+        uid: int
+            The ID of the user you want to update.
+
+        Returns:
+        --------
+        None, but updates self.global.
+        """
+        self.userGlobal[uid] = datetime.now()
+
+    async def flushToDisk(self):
+        """Flush last active data to the disk."""
+        pass
 
 def setup(bot):
     """Add the cog to the bot."""
@@ -63,5 +209,7 @@ def setup(bot):
                                                datefmt="[%d/%m/%Y %H:%M:%S]"))
         LOGGER.addHandler(handler)
     customCog = LastActive(bot)
-    bot.add_listener(customCog.listener, "on_message")
+    bot.add_listener(customCog.listenMessage, "on_message")
+    bot.add_listener(customCog.listenReaction, "on_reaction_add")
+    bot.add_listener(customCog.listenReaction, "on_reaction_remove")
     bot.add_cog(customCog)
