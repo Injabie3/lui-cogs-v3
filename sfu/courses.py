2f26abec7e5a55bc85a3a86487bf3c4cd8504893
Injabie3 <injabie3@gmail.com>
2019-08-25 16:32:09 -0700

    [SFU] Run courses.py through YAPF and DocFormatter

--- sfu/courses.py
+++ sfu/courses.py
@@ -4,16 +4,23 @@ from redbot.core import commands
 from redbot.core.commands.context import Context
 from .api import dictOutline
 
+
 class SFUCourses(commands.Cog):
-    """A cog to search for SFU courses, from the kind souls """
+    """A cog to search for SFU courses, from the kind souls at SFU CSSS."""
+
     # Class constructor
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="course")
     @commands.guild_only()
-    async def lookup(self, ctx: Context, department: str, number: str,
-                     semester: str = None, year: str = None, section: str = None): \
+    async def lookup(self,
+                     ctx: Context,
+                     department: str,
+                     number: str,
+                     semester: str = None,
+                     year: str = None,
+                     section: str = None):         \
                      # pylint: disable=too-many-arguments
         """Displays a course outline.  Defaults to current semester and year.
 
@@ -25,28 +32,38 @@ class SFUCourses(commands.Cog):
             section = 'placeholder'
         if not year:
             year = 'current'
-        if not semester or semester.lower() not in ["fall", "summer", "spring"]:
+        if not semester or semester.lower() not in [
+                "fall", "summer", "spring"
+        ]:
             # self.logger.debug("Invalid semester")
             semester = 'current'
 
         message = await ctx.send(":hourglass: Searching...")
         try:
-            result = await dictOutline(department, number, section, year, semester)
+            result = await dictOutline(department, number, section, year,
+                                       semester)
         except ValueError:
-            await message.edit(content=":warning: This course could not be found! "
-                               "Please retry with different parameters.")
+            await message.edit(
+                content=":warning: This course could not be found! "
+                "Please retry with different parameters.")
             return
 
         embed = discord.Embed(title=result["Title"])
         # print(result)
         embed.add_field(name="Description", value=result["Description"])
-        embed.add_field(name="Details", value="{} [More Info](https://www.sfu.ca/outlines."
-                        "html?{})".format(result["Details"], result["Outline"].lower()))
+        embed.add_field(name="Details",
+                        value="{} [More Info](https://www.sfu.ca/outlines."
+                        "html?{})".format(result["Details"],
+                                          result["Outline"].lower()))
         if result["Prerequisites"]:
-            embed.add_field(name="Prerequisites", value=result["Prerequisites"])
-        embed.add_field(name="Instructor", value=result["Instructor"], inline=False)
+            embed.add_field(name="Prerequisites",
+                            value=result["Prerequisites"])
+        embed.add_field(name="Instructor",
+                        value=result["Instructor"],
+                        inline=False)
         embed.add_field(name="Class Times", value=result["Class Times"])
         embed.add_field(name="Exam Time", value=result["Exam Time"])
-        await message.edit(content=":information_source: Here is the course outline "
-                           "you requested, {}!".format(ctx.message.author.mention),
-                           embed=embed)
+        await message.edit(
+            content=":information_source: Here is the course outline "
+            "you requested, {}!".format(ctx.message.author.mention),
+            embed=embed)
