eb97a259da1d480e34193bc49dadb6409dc5777e
Injabie3 <injabie3@gmail.com>
2019-08-26 18:40:21 -0700

    [SFU] Remove unneeded function for logging folders

--- sfu/__init__.py
+++ sfu/__init__.py
@@ -8,14 +8,9 @@ from redbot.core.bot import Red
 from .courses import SFUCourses
 from .roads import SFURoads
 
-def checkFolders():
-    """Make sure folder for logs is available."""
-    if not os.path.exists(LOG_FOLDER):
-        os.makedirs(LOG_FOLDER)
 
 def setup(bot: Red):
     """Add the cog to the bot."""
-    checkFolders()
     coursesCog = SFUCourses(bot)
     roadsCog = SFURoads(bot)
     bot.add_cog(coursesCog)
