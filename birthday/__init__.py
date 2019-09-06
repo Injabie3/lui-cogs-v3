1d753c280a2c717ac0c723ce7efff867d8dc327a
Injabie3 <injabie3@gmail.com>
2019-09-05 22:56:36 -0700

    [Birthday] Add __init__.py

---
+++ birthday/__init__.py
@@ -0,0 +1,13 @@
+"""birthday module.
+
+Auto-add guild members to a birthday role on their birthday.
+
+This cog requires paginator.py, obtainable from Rapptz/RoboDanny.
+"""
+
+from redbot.core.bot import Red
+from .birthday import Birthday
+
+def setup(bot: Red):
+    """Add the cog to the bot."""
+    bot.add_cog(Birthday(bot))
