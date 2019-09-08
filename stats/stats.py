fdcb7fa89f759c0c74f2d495499a786739070df3
Injabie3 <injabie3@gmail.com>
2019-09-07 22:39:40 -0700

    [Stats] Begin outlining update command - Add relevant default values for channel and guild. - Add stat group command. - Add empty on_message listener.

--- stats/stats.py
+++ stats/stats.py
@@ -4,12 +4,23 @@ Collect some stats for ourselves.
 """
 
 from redbot.core import Config, checks, commands, data_manager
-from redbot.core.utils import paginator
 from redbot.core.bot import Red
+from redbot.core.commands.context import Context
+from redbot.core.utils import paginator
 
 BASE_MEMBER = \
 {
     "messageCount": {}
+}
+
+BASE_GUILD = \
+{
+    "initialized": False,
+}
+
+BASE_CHANNEL = \
+{
+    "lastUpdated": None
 }
 
 
@@ -21,6 +32,8 @@ class Stats(commands.Cog):
                                       identifier=5842647,
                                       force_registration=True)
         self.config.register_member(**BASE_MEMBER)
+        self.config.register_channel(**BASE_CHANNEL)
+        self.config.register_guild(**BASE_GUILD)
 
         # Initialize logger, and save to cog folder.
         saveFolder = data_manager.cog_data_path(cog_instance=self)
@@ -36,3 +49,36 @@ class Stats(commands.Cog):
                 logging.Formatter("%(asctime)s %(message)s",
                                   datefmt="[%d/%m/%Y %H:%M:%S]"))
             self.logger.addHandler(handler)
+
+
+    @commands.group(name="stats")
+    @commands.guild_only()
+    async def statsGroup(self, ctx: Context):
+        """Stats for the guild."""
+        pass
+
+    @stats.command(name="update")
+    async def update(self, ctx: Context):
+        """Update the message count for all users on the guild.
+
+        This operation may take a long time.
+        """
+        self.logger.debug("Starting stats update on guild %s (%s)",
+                          ctx.guild.name, ctx.guild.id)
+        status = ctx.send(":information_source: Updating, please wait...")
+        membersLock = self.config.get_members_lock()
+        channelsLock = self.config.get_channels_lock()
+
+        await membersLock.acquire()
+        await channelsLock.acquire()
+
+        self.logger.debug("Member and channel locks acquired")
+
+        for guild in self.bot.guilds:
+            pass
+
+
+    @commands.Cog.listener("on_message")
+    async def messageListener(self, message: discord.Message):
+        """Check each message and update the info for the guild member."""
+        pass
