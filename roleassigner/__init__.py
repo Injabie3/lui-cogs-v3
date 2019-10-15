2dce50f936b4bd84546d7cdde75e84abcc7c53f6
Injabie3 <injabie3@gmail.com>
2019-10-14 18:04:17 -0700

    [RoleAssigner] Add __init__.py

---
+++ roleassigner/__init__.py
@@ -0,0 +1,11 @@
+"""roleassigner module.
+
+Assign roles to certain guild members.
+"""
+
+from redbot.core.bot import Red
+from .roleassigner import RoleAssigner
+
+def setup(bot: Red):
+    """Add the cog to the bot."""
+    bot.add_cog(RoleAssigner(bot))
