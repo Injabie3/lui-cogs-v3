273eefe8d6938488da7cfc3ada941dc261ef822a
Injabie3 <injabie3@gmail.com>
2020-05-30 14:06:29 -0700

    [Tags] Add __init__.py
    - Basic setup entry.

---
+++ cogs/tags/__init__.py
@@ -0,0 +1,10 @@
+"""Tags module
+Custom commands with ownership, stats, and more.
+"""
+
+from redbot.core.bot import Red
+from .tags import Tags
+
+def setup(bot: Red):
+    """Add the cog to the bot."""
+    bot.add_cog(Tags(bot))
