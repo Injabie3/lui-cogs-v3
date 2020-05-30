273ec04e0e41c842adfcce54ee171941f9b209e9
Injabie3 <injabie3@gmail.com>
2020-05-30 15:23:40 -0700

    [Tags] Fix deprecation warning in config.py

--- tags/config.py
+++ tags/config.py
@@ -32,7 +32,7 @@ class Config:
             self._db = {}
 
     async def load(self):
-        with await self.lock:
+        async with self.lock:
             await self.loop.run_in_executor(None, self.load_from_file)
 
     def _dump(self):
@@ -44,7 +44,7 @@ class Config:
         os.replace(temp, self.directory+self.name)
 
     async def save(self):
-        with await self.lock:
+        async with self.lock:
             await self.loop.run_in_executor(None, self._dump)
 
     def get(self, key, *args):
