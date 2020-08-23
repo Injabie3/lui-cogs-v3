2c1818ebef8dfc27b3980af2a504453c4465f7d4
Injabie3 <injabie3@gmail.com>
2020-08-22 21:31:01 -0700

    [Birthday] Move constants to another file

--- birthday/birthday.py
+++ birthday/birthday.py
@@ -9,25 +9,7 @@ from redbot.core import Config, checks, commands, data_manager
 from redbot.core.commands.context import Context
 from redbot.core.utils import paginator
 from redbot.core.bot import Red
-
-# Global variables
-KEY_BDAY_ROLE = "birthdayRole"
-KEY_BDAY_USERS = "birthdayUsers"
-KEY_BDAY_MONTH = "birthdateMonth"
-KEY_BDAY_DAY = "birthdateDay"
-KEY_IS_ASSIGNED = "isAssigned"
-KEY_DATE_SET_MONTH = "dateAssignedMonth"
-KEY_DATE_SET_DAY = "dateAssignedDay"
-
-BASE_GUILD_MEMBER = {
-    KEY_BDAY_DAY: None,
-    KEY_BDAY_MONTH: None,
-    KEY_DATE_SET_DAY: None,
-    KEY_DATE_SET_MONTH: None,
-    KEY_IS_ASSIGNED: False,
-}
-
-BASE_GUILD = {KEY_BDAY_ROLE: None}
+from .constants import *
 
 
 class Birthday(commands.Cog):
---
+++ birthday/constants.py
@@ -0,0 +1,17 @@
+KEY_BDAY_ROLE = "birthdayRole"
+KEY_BDAY_USERS = "birthdayUsers"
+KEY_BDAY_MONTH = "birthdateMonth"
+KEY_BDAY_DAY = "birthdateDay"
+KEY_IS_ASSIGNED = "isAssigned"
+KEY_DATE_SET_MONTH = "dateAssignedMonth"
+KEY_DATE_SET_DAY = "dateAssignedDay"
+
+BASE_GUILD_MEMBER = {
+    KEY_BDAY_DAY: None,
+    KEY_BDAY_MONTH: None,
+    KEY_DATE_SET_DAY: None,
+    KEY_DATE_SET_MONTH: None,
+    KEY_IS_ASSIGNED: False,
+}
+
+BASE_GUILD = {KEY_BDAY_ROLE: None}
