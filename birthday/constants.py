7891314479a3be7277db43ce394a2e8039ce34d2
Injabie3 <injabie3@gmail.com>
2020-10-06 21:40:54 -0700

    [Birthday] Add additional constants
    - Add birthdayChannel attribute for birthday mentions (to come).
    - Add attribute to base guild.
    - Add a list of canned messages for mentions.

--- birthday/constants.py
+++ birthday/constants.py
@@ -1,3 +1,4 @@
+KEY_BDAY_CHANNEL = "birthdayChannel"
 KEY_BDAY_ROLE = "birthdayRole"
 KEY_BDAY_USERS = "birthdayUsers"
 KEY_BDAY_MONTH = "birthdateMonth"
@@ -14,4 +15,15 @@ BASE_GUILD_MEMBER = {
     KEY_IS_ASSIGNED: False,
 }
 
-BASE_GUILD = {KEY_BDAY_ROLE: None}
+BASE_GUILD = {
+    KEY_BDAY_CHANNEL: None,
+    KEY_BDAY_ROLE: None
+}
+
+CANNED_MESSAGES = [
+    "Wow look, it's {}'s birthday today! Happy birthday, hope you have a good one!",
+    "お誕生日おめでとう、{}! (TL note: Happy birthday!)",
+    "Wow, would you look at that, it's {}'s birthday. Time to wish them a happy birthday!",
+    "Hey everyone! it's {} birthday, come and wish them a happy birthday!",
+    "Wow {}, happy birthday! How does it feel to be more boomer than you were yesterday?"
+]
