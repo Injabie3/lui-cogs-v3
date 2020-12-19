e6924509ad23ab0f680d15955b3ff45ce9d56131
Injabie3 <injabie3@gmail.com>
2020-12-18 17:58:18 -0800

    [Ranks] Add cog loader

---
+++ ranks/__init__.py
@@ -0,0 +1,10 @@
+"""Ranks cog.
+Keep track of active members on the server.
+"""
+
+from redbot.core.bot import Red
+from .ranks import Ranks
+
+def setup(bot: Red):
+    """Add the cog to the bot."""
+    bot.add_cog(Ranks(bot))
