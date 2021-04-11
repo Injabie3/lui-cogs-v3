17e79967488c7d976acb8fe37e225978cdfe71e5
Injabie3 <injabie3@gmail.com>
2021-04-10 21:03:13 -0700

    [Tags] Sync roles when bot connects and has all info

--- tags/tags.py
+++ tags/tags.py
@@ -166,7 +166,14 @@ class Tags(commands.Cog):
         self.lock = Lock()
         tagGroup = self.get_commands()[0]
         self.tagCommands = tagGroup.all_commands.keys()
-        self.bot.loop.create_task(self.syncAllowedRoles())
+        self.syncLoopCreated = False
+
+    @commands.Cog.listener("on_ready")
+    async def initialSyncLoop(self):
+        if not self.syncLoopCreated:
+            # TODO Add logger here.
+            self.bot.loop.create_task(self.syncAllowedRoles())
+            self.syncLoopCreated = True
 
     async def syncAllowedRoles(self):
         guildIds = await self.configV3.all_guilds()
