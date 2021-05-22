d4f7a9a5a5974b307f69994a11ff6a8bb59111d2
tplamondon <tplamondon@tutanota.de>
2021-05-21 22:38:54 -0700

    [Welcome] Add public welcome message (SFUAnime/Ren#389)
    * .gitignore to ignore my ren stuff
    
    * Revert ".gitignore to ignore my ren stuff"
    
    This reverts commit a5f272498236485e3c7e1bd2a2c0a33c4896e552.
    
    * started work on greet cog
    
    * changed name of one method
    
    * finished greetCommand hopefully
    
    * finished getRandomMessage method
    
    * added checks.mod to greetAdd
    
    * last commit, things before this get bonked
    
    * finished add greeting
    
    * added list
    
    * ironed out some bugs with add, remove, list
    
    * deleted unneccary old work, couldn't figure out how to undo commits
    
    * added set welcome channel
    
    * added sendwelcomemessagechannel
    
    * got mentioning to work correctly
    
    * added some extra message stuff based on ono's requirements
    
    * updated dosctrings
    
    * formatted with black
    
    * updated key "greetings" -> "KEY_GREETINGS"
    
    * changed keys and name of one
    
    * renabled pm's for welcome
    
    * changed if numGreetings == 0 to if not numGreetings
    
    * if len(greetings) == 0: -> if not greetings:
    
    * pulled guild pass out to the method
    
    * return greetings[random.randint(0, numGreetings - 1)][1] to random.choice()[1]
    
    * removed last guild pass
    
    * removed unnecessary import
    
    * added type hints to greet add
    
    * added more type hints
    
    * added aliases to remove command
    
    * alias to list and added type hint
    
    * removed hardcoding, sorry ono. you have to type all that now
    
    * switched to using dictionary instead of list of tuples
    
    * added todo to remove later
    
    * added logging
    
    * fixed issue with random selection from dictionary
    
    * fixed list command
    
    * replaced .split() and manually putting in pings with .replace
    
    * removed todo send test
    
    * removed code that was meant to be removed earlier
    
    * removed error channel stuff
    
    * fixed compile error with missing comma
    
    * readded back code that should be there
    
    * gave logging info into removed chanenl
    
    * ran black
    
    * updated add doctstring
    
    * updated docstrings
    
    * removed unneeded check
    
    * added way to remove welcome channel
    
    * updated list command's for loops to search greetings
    
    * docstring of list updated
    
    * updated remove docstring
    
    * fixed coglistener to properly check for deleted channels I hope
    
    * ran black again
    
    * Server greetings changes -> Welcome greetings changes
    
    * redid max message length check to use constant and temp variable
    
    * changed to greetings group for new stuff
    
    * nested greetings in welcomeset
    
    * stirng of 0's to ctx.author.mention
    
    Co-authored-by: Lui <injabie3@gmail.com>
    
    * contents -> content line 256
    
    Co-authored-by: Lui <injabie3@gmail.com>

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -5,10 +5,14 @@ Sends welcome DMs to users that join the server.
 import asyncio
 import discord
 import logging
+import random
 
 from redbot.core import Config, checks, commands
 from redbot.core.bot import Red
 from redbot.core.commands.context import Context
+from redbot.core.utils.chat_formatting import error, pagify, warning
+from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
+from redbot.core.utils import AsyncIter
 
 LOGGER = logging.getLogger("red.luicogs.Welcome")
 
@@ -20,6 +24,11 @@ KEY_LOG_LEAVE_CHANNEL = "logLeaveChannel"
 KEY_TITLE = "title"
 KEY_MESSAGE = "message"
 KEY_IMAGE = "image"
+KEY_GREETINGS = "greetings"
+KEY_WELCOME_CHANNEL = "welcomeChannel"
+KEY_WELCOME_CHANNEL_ENABLED = "welcomeChannelSet"
+
+MAX_MESSAGE_LENGTH = 2000
 
 DEFAULT_GUILD = {
     KEY_DM_ENABLED: False,
@@ -30,6 +39,9 @@ DEFAULT_GUILD = {
     KEY_TITLE: "Welcome!",
     KEY_MESSAGE: "Welcome to the server! Hope you enjoy your stay!",
     KEY_IMAGE: None,
+    KEY_GREETINGS: {},
+    KEY_WELCOME_CHANNEL: None,
+    KEY_WELCOME_CHANNEL_ENABLED: False,
 }
 
 
@@ -42,14 +54,70 @@ class Welcome(commands.Cog):  # pylint: disable=too-many-instance-attributes
         self.config = Config.get_conf(self, identifier=5842647, force_registration=True)
         self.config.register_guild(**DEFAULT_GUILD)
 
+    async def getRandomMessage(self, guild):
+        greetings = await self.config.guild(guild).greetings()
+        numGreetings = len(greetings)
+        if not greetings:
+            return "Welcome to the server {USER}"
+        else:
+            return random.choice(list(greetings.values()))
+
     # The async function that is triggered on new member join.
     @commands.Cog.listener()
     async def on_member_join(self, newMember: discord.Member):
+        await self.sendWelcomeMessageChannel(newMember)
         await self.sendWelcomeMessage(newMember)
 
     @commands.Cog.listener()
     async def on_member_remove(self, leaveMember: discord.Member):
         await self.logServerLeave(leaveMember)
+
+    # This async function is to look for if the welcome channel was removed
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, removedChannel: discord.TextChannel):
+        if not isinstance(removedChannel, discord.TextChannel):
+            return
+        guild = removedChannel.guild
+        # the channel to post welcome stuff in
+        welcomeIDSet = await self.config.guild(guild).welcomeChannelSet()
+        welcomeID = await self.config.guild(guild).welcomeChannel()
+        if welcomeIDSet and removedChannel.id == welcomeID:
+            await self.config.guild(guild).welcomeChannelSet.set(False)
+            LOGGER.info("Changed guild's welcomeChannelSetFlag to false as channel was deleted")
+        return
+
+    async def sendWelcomeMessageChannel(self, newUser: discord.Member):
+        guild = newUser.guild
+        channelID = await self.config.guild(guild).welcomeChannel()
+        isSet = await self.config.guild(guild).welcomeChannelSet()
+        # if channel isn't set
+        if not isSet:
+            return
+        channel = discord.utils.get(guild.channels, id=channelID)
+        rawMessage = await self.getRandomMessage(guild)
+
+        message = rawMessage.replace("{USER}", newUser.mention)
+        splitMessage = rawMessage.split("{USER}")
+
+        try:
+            await channel.send(message)
+        except (discord.Forbidden, discord.HTTPException) as errorMsg:
+            LOGGER.error(
+                "Could not send message, please make sure the bot "
+                "has enough permissions to send messages to this "
+                "channel!",
+                exc_info=True,
+            )
+            LOGGER.error(errorMsg)
+        else:
+            LOGGER.info(
+                "User %s#%s (%s) has joined. Posted welcome message.",
+                newUser.name,
+                newUser.discriminator,
+                newUser.id,
+            )
+
+        return
 
     async def sendWelcomeMessage(self, newUser, test=False):
         """Sends the welcome message in DM."""
@@ -132,6 +200,153 @@ class Welcome(commands.Cog):  # pylint: disable=too-many-instance-attributes
     async def welcome(self, ctx: Context):
         """Server welcome message settings."""
 
+    # [p]greetings
+    @welcome.group(name="greetings")
+    @commands.guild_only()
+    @checks.mod_or_permissions()
+    async def greetings(self, ctx: Context):
+        """Server greetings message settings."""
+
+    @checks.mod_or_permissions()
+    @greetings.command(name="channel")
+    async def welcomeChannelSet(self, ctx: Context, channel: discord.TextChannel = None):
+        """
+        Set the welcome channel
+
+        Parameters:
+        -----------
+        channel: The text channel to set welcome's to. If not passed anything, will remove the welcome channel
+        """
+        if not channel:
+            # channel == None
+            await self.config.guild(ctx.guild).welcomeChannelSet.set(False)
+            await ctx.send("Welcome channel has been removed")
+            return
+
+        await self.config.guild(ctx.guild).welcomeChannel.set(channel.id)
+        await self.config.guild(ctx.guild).welcomeChannelSet.set(True)
+        await ctx.send(f"Channel set to {channel}")
+
+        return
+
+    @checks.mod_or_permissions()
+    @greetings.command(name="add")
+    async def greetAdd(self, ctx: Context, name: str):
+        """
+        Add a new greeting
+
+        Including {USER} in the message will have that replaced with a ping to the new user
+
+        Parameters:
+        -----------
+        name: name of the greeting
+        """
+
+        def check(message: discord.Message):
+            return message.author == ctx.message.author and message.channel == ctx.message.channel
+
+        await ctx.send("What would you like the greeting message to be?")
+        try:
+            greeting = await self.bot.wait_for("message", check=check, timeout=30.0)
+        except asyncio.TimeoutError:
+            await ctx.send("No response received, not setting anything!")
+            return
+
+        # check for if the message with a replaced {USER} will be too long
+        tempmsg = greeting.content.replace("{USER}", ctx.author.mention)
+        if len(tempmsg) > MAX_MESSAGE_LENGTH:
+            await ctx.send("Your message is too long!")
+            return
+
+        greetings = await self.config.guild(ctx.guild).greetings()
+
+        if name in greetings:
+            await ctx.send(
+                warning(
+                    "This greeting already exists, overwrite it? Please type 'yes' to overwrite"
+                )
+            )
+            try:
+                response = await self.bot.wait_for("message", timeout=30.0, check=check)
+            except asyncio.TimeoutError:
+                await ctx.send("You took too long, not overwriting")
+                return
+
+            if response.content.lower() != "yes":
+                await ctx.send("Not overwriting the greeting")
+                return
+
+        # save the greetings
+        greetings[name] = greeting.content
+        await greeting.add_reaction("✅")
+        await self.config.guild(ctx.guild).greetings.set(greetings)
+        return
+
+    @checks.mod_or_permissions()
+    @greetings.command(name="remove", aliases=["delete", "del", "rm"])
+    async def greetRemove(self, ctx: Context, name: str):
+        """
+        Remove a greeting
+
+        Parameters:
+        -----------
+        name: name of the greeting to remove
+        """
+
+        def check(message: discord.Message):
+            return message.author == ctx.message.author and message.channel == ctx.message.channel
+
+        greetings = await self.config.guild(ctx.guild).greetings()
+        if name in greetings:
+            await ctx.send(
+                warning("Are you sure you wish to delete this greeting? Respond with 'yes' if yes")
+            )
+            try:
+                response = await self.bot.wait_for("message", timeout=30.0, check=check)
+            except asyncio.TimeoutError:
+                await ctx.send("You took too long, not deleting")
+                return
+
+            if response.content.lower() != "yes":
+                await ctx.send("Not deleting")
+                return
+
+            # delete the greeting
+            del greetings[name]
+            await ctx.send(f"{name} removed from list")
+            await self.config.guild(ctx.guild).greetings.set(greetings)
+        else:
+            ctx.send(f"{name} not in list of greetings")
+        return
+
+    @checks.mod_or_permissions()
+    @greetings.command(name="list", aliases=["ls"])
+    async def greetList(self, ctx: Context):
+        """
+        List all greetings on the server
+        """
+        greetings = await self.config.guild(ctx.guild).greetings()
+
+        if not greetings:
+            await ctx.send("There are no greetings, please add some first!")
+            return
+
+        msg = ""
+
+        for name, greeting in greetings.items():
+            msg += f"{name}: {greeting}\n"
+
+        pageList = []
+        pages = list(pagify(msg, page_length=500))
+        totalPages = len(pages)
+        async for pageNumber, page in AsyncIter(pages).enumerate(start=1):
+            embed = discord.Embed(
+                title=f"Welcome greetings changes for {ctx.guild.name}", description=page
+            )
+            embed.set_footer(text=f"Page {pageNumber}/{totalPages}")
+            pageList.append(embed)
+        await menu(ctx, pageList, DEFAULT_CONTROLS)
+
     # [p]welcome setmessage
     @welcome.command(name="message", aliases=["msg"])
     async def setmessage(self, ctx: Context):
@@ -147,7 +362,7 @@ class Welcome(commands.Cog):  # pylint: disable=too-many-instance-attributes
             await ctx.send("No response received, not setting anything!")
             return
 
-        if len(message.content) > 2048:
+        if len(message.content) > MAX_MESSAGE_LENGTH:
             await ctx.send("Your message is too long!")
             return
 
