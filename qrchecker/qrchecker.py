05597c11f46b232a2608ba3c68a49b58e203a562
Injabie3 <injabie3@gmail.com>
2021-08-29 14:49:43 -0700

    [QRChecker] Reduce logger verbosity

--- qrchecker/qrchecker.py
+++ qrchecker/qrchecker.py
@@ -13,7 +13,7 @@ class QRChecker(commands.Cog):
     def __init__(self, bot: Red):
         self.bot = bot
         self.logger = logging.getLogger("red.luicogs.QRChecker")
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.INFO)
 
     @commands.Cog.listener("on_message")
     async def listener(self, message: discord.Message):
