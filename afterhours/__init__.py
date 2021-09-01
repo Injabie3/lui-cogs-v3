e34535f89d0b7f5f3fb722e7b5bc1906dda764b9
Injabie3 <injabie3@gmail.com>
2021-08-31 22:08:36 -0700

    [AfterHours] Add blank line
    - Satisfy the gods that is black.

--- afterhours/__init__.py
+++ afterhours/__init__.py
@@ -12,6 +12,7 @@ from .afterhours import AfterHours
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
+
 def setup(bot: Red):
     """Add the cog to the bot."""
     ahCog = AfterHours(bot)
