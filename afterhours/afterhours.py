0f4d2afe729a811cc3c49b3a2f23b7043815d5f1
Injabie3 <injabie3@gmail.com>
2021-09-04 15:01:33 -0700

    [AfterHours] Use OS-independent paths

--- afterhours/afterhours.py
+++ afterhours/afterhours.py
@@ -2,6 +2,7 @@
 
 A special cog to handle the special cases for this channel.
 """
+import os
 import re
 import logging
 import asyncio
@@ -58,9 +59,8 @@ class AfterHours(commands.Cog):
         if self.logger.level == 0:
             # Prevents the self.logger from being loaded again in case of module reload.
             self.logger.setLevel(logging.INFO)
-            handler = logging.FileHandler(
-                filename=str(saveFolder) + "/info.log", encoding="utf-8", mode="a"
-            )
+            logPath = os.path.join(saveFolder, "info.log")
+            handler = logging.FileHandler(filename=logPath, encoding="utf-8", mode="a")
             handler.setFormatter(
                 logging.Formatter("%(asctime)s %(message)s", datefmt="[%d/%m/%Y %H:%M:%S]")
             )
