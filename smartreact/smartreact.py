c0244bd8ffb8a9777787db9e29d8f8381149f54b
Injabie3 <injabie3@gmail.com>
2021-09-04 16:22:04 -0700

    [SmartReact] Use OS-independent paths

--- smartreact/smartreact.py
+++ smartreact/smartreact.py
@@ -3,6 +3,7 @@
 This cog was originally from flapjax/FlapJack-Cogs in v2.
 """
 import logging
+import os
 import re
 import asyncio
 import discord
@@ -34,9 +35,8 @@ class SmartReact(commands.Cog):
         if self.logger.level == 0:
             # Prevents the self.logger from being loaded again in case of module reload.
             self.logger.setLevel(logging.INFO)
-            handler = logging.FileHandler(
-                filename=str(saveFolder) + "/info.log", encoding="utf-8", mode="a"
-            )
+            logPath = os.path.join(saveFolder, "info.log")
+            handler = logging.FileHandler(filename=logPath, encoding="utf-8", mode="a")
             handler.setFormatter(
                 logging.Formatter("%(asctime)s %(message)s", datefmt="[%d/%m/%Y %H:%M:%S]")
             )
