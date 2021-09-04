ce31ee01b3314d85f4f6bc15e24b042933ee52e6
Injabie3 <injabie3@gmail.com>
2021-09-04 15:50:59 -0700

    [RoleAssigner] Use OS-independent paths

--- roleassigner/roleassigner.py
+++ roleassigner/roleassigner.py
@@ -4,6 +4,7 @@ Randomly assigns roles to users.
 """
 
 import logging
+import os
 import random
 import discord
 from redbot.core import Config, checks, commands, data_manager
@@ -26,9 +27,8 @@ class RoleAssigner(commands.Cog):
         if self.logger.level == 0:
             # Prevents the self.logger from being loaded again in case of module reload.
             self.logger.setLevel(logging.INFO)
-            handler = logging.FileHandler(
-                filename=str(saveFolder) + "/info.log", encoding="utf-8", mode="a"
-            )
+            logPath = os.path.join(saveFolder, "info.log")
+            handler = logging.FileHandler(filename=logPath, encoding="utf-8", mode="a")
             handler.setFormatter(
                 logging.Formatter("%(asctime)s %(message)s", datefmt="[%d/%m/%Y %H:%M:%S]")
             )
