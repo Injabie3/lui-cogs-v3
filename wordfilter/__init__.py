286b3b744f4ede2f6a39060fdb05c2620494f818
Injabie3 <injabie3@gmail.com>
2022-04-18 16:51:17 -0700

    [WordFilter] Modify for Red-DiscordBot dpy2 support
    - add_cog is now a coroutine.

--- wordfilter/__init__.py
+++ wordfilter/__init__.py
@@ -13,7 +13,7 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot: Red):
+async def setup(bot: Red):
     """Add the cog to the bot."""
     wordFilterCog = WordFilter(bot)
-    bot.add_cog(wordFilterCog)
+    await bot.add_cog(wordFilterCog)
