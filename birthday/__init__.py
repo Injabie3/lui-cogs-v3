79af962fa7efecd9a00ac7e7d31164efb0377045
Injabie3 <injabie3@gmail.com>
2022-04-25 22:26:39 -0700

    [Birthday] Modify for Red-DiscordBot dpy2 support
    - add_cog is now a coroutine.
    - Verified that birthday background loop still works as expected.

--- birthday/__init__.py
+++ birthday/__init__.py
@@ -12,6 +12,6 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot: Red):
+async def setup(bot: Red):
     """Add the cog to the bot."""
-    bot.add_cog(Birthday(bot))
+    await bot.add_cog(Birthday(bot))
