917f17b474018e91ea18514947488fde5883c650
Dat Quach <quachtridat@yahoo.com.vn>
2022-05-04 18:52:11 -0700

    [Respects] Modify for Red-DiscordBot dpy2 support
    - add_cog is now a coroutine.

--- respects/__init__.py
+++ respects/__init__.py
@@ -12,7 +12,7 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot: Red):
+async def setup(bot: Red):
     """Add the cog to the bot."""
     customCog = Respects(bot)
-    bot.add_cog(customCog)
+    await bot.add_cog(customCog)
