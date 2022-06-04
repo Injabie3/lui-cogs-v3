d4aff96656e11da81e3efe9083426acb5d6c0069
Injabie3 <injabie3@gmail.com>
2022-06-04 11:31:56 -0700

    [GoodSmileInfo] Simplify logging setup
    - Remove explicit logging level configuration, take parent.
    - Gate handler adding by checking if a handler was already added.

--- goodsmileinfo/gscinfo.py
+++ goodsmileinfo/gscinfo.py
@@ -35,9 +35,7 @@ class GoodSmileInfo(commands.Cog):
         # Initialize logger, and save to cog folder.
         saveFolder = data_manager.cog_data_path(cog_instance=self)
         self.logger = logging.getLogger("red.luicogs.GoodSmileInfo")
-        if self.logger.level == 0:
-            # Prevents the self.logger from being loaded again in case of module reload.
-            self.logger.setLevel(logging.INFO)
+        if not self.logger.handlers:
             logPath = os.path.join(saveFolder, "info.log")
             handler = logging.FileHandler(filename=logPath, encoding="utf-8", mode="a")
             handler.setFormatter(
