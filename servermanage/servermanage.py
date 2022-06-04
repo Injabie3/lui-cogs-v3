f8bfb3f173cc72a13c68e026bd14b84d6c76760e
Injabie3 <injabie3@gmail.com>
2022-06-04 11:45:18 -0700

    [ServerManage] Simplify logging setup
    - Remove explicit logging level configuration, take parent.
    - Gate handler adding by checking if a handler was already added.

--- servermanage/servermanage.py
+++ servermanage/servermanage.py
@@ -33,9 +33,7 @@ class ServerManage(ServerManageCommands, commands.Cog, metaclass=ServerManageMet
         # Initialize logger, and save to cog folder.
         self.dataFolder = data_manager.cog_data_path(cog_instance=self)
         self.logger = logging.getLogger("red.luicogs.ServerManage")
-        if self.logger.level == 0:
-            # Prevents the self.logger from being loaded again in case of module reload.
-            self.logger.setLevel(logging.INFO)
+        if not self.logger.handlers:
             logPath = pathJoin(self.dataFolder, "info.log")
             handler = logging.FileHandler(filename=logPath, encoding="utf-8", mode="a")
             handler.setFormatter(
