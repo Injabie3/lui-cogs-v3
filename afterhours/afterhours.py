36abc4a52bb48ade2180f46c0be573625a678b59
Injabie3 <injabie3@gmail.com>
2022-06-04 11:28:15 -0700

    [AfterHours] Simplify logging setup
    - Remove explicit logging level configuration, take parent.
    - Gate handler adding by checking if a handler was already added.

--- afterhours/afterhours.py
+++ afterhours/afterhours.py
@@ -56,9 +56,7 @@ class AfterHours(commands.Cog):
         # Initialize logger, and save to cog folder.
         saveFolder = data_manager.cog_data_path(cog_instance=self)
         self.logger = logging.getLogger("red.luicogs.AfterHours")
-        if self.logger.level == 0:
-            # Prevents the self.logger from being loaded again in case of module reload.
-            self.logger.setLevel(logging.INFO)
+        if not self.logger.handlers:
             logPath = os.path.join(saveFolder, "info.log")
             handler = logging.FileHandler(filename=logPath, encoding="utf-8", mode="a")
             handler.setFormatter(
