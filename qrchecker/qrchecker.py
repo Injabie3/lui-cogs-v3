19fbaa18ba5462d1d7bb410050daea4139290395
quachtridat <quachtridat@yahoo.com.vn>
2022-06-11 12:47:29 -0700

    [QRChecker] Add type hints
    Also adjust some imports to shorten type hints.

--- qrchecker/qrchecker.py
+++ qrchecker/qrchecker.py
@@ -4,33 +4,35 @@ Requires:
 - pyzbar from PyPI
 - libzbar0 from your distro's package repo
 """
-import io
-from typing import List
-import logging
+from io import BytesIO
+from typing import Dict, List, Optional
+from logging import Logger, getLogger
 
-import discord
+from discord import AllowedMentions, Guild, Message
 from pyzbar.pyzbar import Decoded, decode, ZBarSymbol
 from PIL import Image
 from redbot.core import commands, Config
 from redbot.core.bot import Red
+from redbot.core.commands import Context
+from redbot.core.config import Group
 from redbot.core.utils.chat_formatting import box, pagify
 
-KEY_ENABLED = "enabled"
+KEY_ENABLED: str = "enabled"
 
-BASE_GUILD = {KEY_ENABLED: False}
+BASE_GUILD: Dict = {KEY_ENABLED: False}
 
 
 class QRChecker(commands.Cog):
     """A QR code checker for attachments"""
 
     def __init__(self, bot: Red):
-        self.bot = bot
-        self.logger = logging.getLogger("red.luicogs.QRChecker")
-        self.config = Config.get_conf(self, identifier=5842647, force_registration=True)
+        self.bot: Red = bot
+        self.logger: Logger = getLogger("red.luicogs.QRChecker")
+        self.config: Config = Config.get_conf(self, identifier=5842647, force_registration=True)
         self.config.register_guild(**BASE_GUILD)
 
     @commands.Cog.listener("on_message")
-    async def listener(self, message: discord.Message):
+    async def listener(self, message: Message):
         """Find QR code in message attachments"""
         if not message.guild:
             return
@@ -46,7 +48,7 @@ class QRChecker(commands.Cog):
             self.logger.debug("No attachments, return early")
             return
         for attachment in message.attachments:
-            contentType = attachment.content_type
+            contentType: Optional[str] = attachment.content_type
             if not contentType:
                 self.logger.debug("Unknown content type, continue")
                 continue
@@ -56,8 +58,8 @@ class QRChecker(commands.Cog):
 
             # At this point we decern that it's an image.
             try:
-                fp = io.BytesIO(await attachment.read())
-                image = Image.open(fp)
+                fp: BytesIO = BytesIO(await attachment.read())
+                image: Image = Image.open(fp)
                 codes: List[Decoded] = decode(image, symbols=[ZBarSymbol.QRCODE])
                 self.logger.debug("Found %s codes", len(codes))
             except Exception:
@@ -79,37 +81,37 @@ class QRChecker(commands.Cog):
 
             numQrCodes = len(codes)
             if numQrCodes == 1:
-                code = codes[0]
-                data = code.data.decode()
+                code: Decoded = codes[0]
+                data: str = code.data.decode()
                 if len(data) == 0:
                     self.logger.debug("No data in QR code.")
                     return
                 if len(data) > 1900:
-                    contents = f"{data[:1900]}..."
+                    contents: str = f"{data[:1900]}..."
                 else:
-                    contents = data
+                    contents: str = data
                 msg = (
                     f"Found a QR code from {message.author.mention}, "
                     f"the contents are: {box(contents)}"
                 )
                 await message.reply(
-                    msg, mention_author=False, allowed_mentions=discord.AllowedMentions.none()
+                    msg, mention_author=False, allowed_mentions=AllowedMentions.none()
                 )
             else:
-                hasData = False
+                hasData: bool = False
                 pages: List[str] = []
                 pages.append(
                     f"Found several QR codes from {message.author.mention}, their contents are:"
                 )
                 for code in codes:
-                    data = code.data.decode()
+                    data: str = code.data.decode()
                     if len(data) == 0:
                         self.logger.debug("No data in QR code.")
                         continue
                     if len(data) > 1990:
-                        contents = f"{box(data[:1990])}..."
+                        contents: str = f"{box(data[:1990])}..."
                     else:
-                        contents = f"{box(data)}"
+                        contents: str = f"{box(data)}"
                     pages.append(contents)
                     hasData |= True
 
@@ -117,42 +119,42 @@ class QRChecker(commands.Cog):
                     self.logger.debug("No data in %s QR codes.", numQrCodes)
                     return
 
-                firstMessage = True
-                sentMessages = 0
+                firstMessage: bool = True
+                sentMessages: int = 0
 
-                ctx = await self.bot.get_context(message)
+                ctx: Context = await self.bot.get_context(message)
                 for textToSend in pagify("\n".join(pages), escape_mass_mentions=True):
                     if firstMessage:
                         await message.reply(
                             textToSend,
                             mention_author=False,
-                            allowed_mentions=discord.AllowedMentions.none(),
+                            allowed_mentions=AllowedMentions.none(),
                         )
                         firstMessage = False
                     elif sentMessages > 10:
                         self.logger.debug("Sent more than 10 messages, bail early")
                         break
                     else:
-                        await ctx.send(textToSend, allowed_mentions=discord.AllowedMentions.none())
+                        await ctx.send(textToSend, allowed_mentions=AllowedMentions.none())
                     sentMessages += 1
 
     @commands.group(name="qrchecker")
     @commands.guild_only()
     @commands.admin_or_permissions(manage_guild=True)
-    async def qrchecker(self, ctx: commands.Context):
+    async def qrchecker(self, ctx: Context):
         """Configure QR code checker"""
         pass
 
     # toggle command
     @qrchecker.command(name="toggle")
-    async def qrcheckerToggle(self, ctx: commands.Context):
+    async def qrcheckerToggle(self, ctx: Context):
         """Toggle QR code checking"""
-        guild = ctx.guild
+        guild: Guild = ctx.guild
         if not guild:
             return
-        guildConfig = self.config.guild(guild)
+        guildConfig: Group = self.config.guild(guild)
 
-        enabled = await guildConfig.get_attr(KEY_ENABLED)()
+        enabled: bool = await guildConfig.get_attr(KEY_ENABLED)()
         if enabled:
             await guildConfig.get_attr(KEY_ENABLED).set(False)
             await ctx.send("QR code checking is now **disabled** for this guild.")
