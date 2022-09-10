920de5d776fabbd4912cb97c8c10f9ee43484bc5
Injabie3 <injabie3@gmail.com>
2022-09-09 23:33:04 -0700

    [Birthday] Refactor test to use parametrized list

--- birthday/testConverters.py
+++ birthday/testConverters.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+from contextlib import nullcontext as doesNotRaise
 from datetime import datetime
 import locale
+from typing import ContextManager
 
 locale.setlocale(locale.LC_ALL, ("en", "UTF-8"))
 
@@ -14,42 +16,36 @@ from .converters import MonthDayConverter
 @pytest.mark.asyncio
 class TestMonthDayConverter:
     converter = MonthDayConverter()
+    raiseNothing = doesNotRaise()
+    raiseBadArgument = pytest.raises(commands.BadArgument)
 
-    @staticmethod
-    def verifyFeb29(bday: datetime):
-        assert bday.month == 2
-        assert bday.day == 29
-
-    @staticmethod
-    def verifyFeb3(bday: datetime):
-        assert bday.month == 2
-        assert bday.day == 3
-
-    @pytest.mark.parametrize("dateString", ("29 Feb", "29 February"))
-    async def testDayTextMonth(self, dateString):
-        bday = await self.converter.convert(None, dateString)
-        self.verifyFeb29(bday)
-
-    @pytest.mark.parametrize("inputString", ("February 30", "some random text"))
-    async def testInvalidDate(self, inputString):
-        with pytest.raises(commands.BadArgument) as excInfo:
-            await self.converter.convert(None, inputString)
-        assert "Invalid date!" in str(excInfo.value)
-
-    async def testTime(self):
-        with pytest.raises(commands.BadArgument) as excInfo:
-            await self.converter.convert(None, "February 29 00:01")
-        assert "Time information should not be supplied!" in str(excInfo.value)
-
-    @pytest.mark.parametrize("dateString", ("02 03", "02 3", "2 03", "2 3"))
-    async def testNumMonthDay(self, dateString):
-        bday = await self.converter.convert(None, dateString)
-        self.verifyFeb3(bday)
-
-    @pytest.mark.parametrize("dateString", ("February 29", "Feb 29"))
-    async def testTextMonthDay(self, dateString):
-        bday = await self.converter.convert(None, dateString)
-        self.verifyFeb29(bday)
+    @pytest.mark.parametrize(
+        ["dateString", "expectM", "expectD", "ctxMgr", "excString"],
+        [
+            ("2 3", 2, 3, raiseNothing, None),
+            ("2 03", 2, 3, raiseNothing, None),
+            ("02 3", 2, 3, raiseNothing, None),
+            ("02 03", 2, 3, raiseNothing, None),
+            ("29 Feb", 2, 29, raiseNothing, None),
+            ("29 February", 2, 29, raiseNothing, None),
+            ("Feb 29", 2, 29, raiseNothing, None),
+            ("February 29", 2, 29, raiseNothing, None),
+            ("some random text", None, None, raiseBadArgument, "Invalid date!"),
+            ("February 30", None, None, raiseBadArgument, "Invalid date!"),
+            ("February 29 00:01", None, None, raiseBadArgument,
+             "Time information should not be supplied!"),
+        ]
+    )
+    async def testInputs(
+            self, dateString: str, expectM: int, expectD: int, ctxMgr: ContextManager,
+            excString: str):
+        with ctxMgr as excInfo:
+            bday = await self.converter.convert(None, dateString)
+            if excString:
+                assert excString in str(excInfo.value)
+            else:
+                assert bday.month == expectM
+                assert bday.day == expectD
 
 
 locale.resetlocale(locale.LC_ALL)
