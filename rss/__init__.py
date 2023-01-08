4d7c579719b78181772b941590ab0d3f7f740cdb
Injabie3 <injabie3@gmail.com>
2023-01-07 21:22:59 -0800

    [RSS] Modify for Red-DiscordBot dpy2 support
    - add_cog is now a coroutine.
    - Verified that RSS background loop still works.

--- rss/__init__.py
+++ rss/__init__.py
@@ -11,8 +11,8 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot: Red):
+async def setup(bot: Red):
     """Add the cog to the bot."""
     rssCog = RSSFeed(bot)
-    bot.add_cog(rssCog)
+    await bot.add_cog(rssCog)
     bot.loop.create_task(rssCog.rss())
