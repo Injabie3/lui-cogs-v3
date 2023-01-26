a356ac47cb84d2b2a908a61bddde05ae6565fc73
quachtridat <quachtridat@yahoo.com.vn>
2023-01-26 01:37:52 -0800

    [Triggered] Add type hint in `__init__.py`
    Add type hint for kwarg `bot` in `setup()` in `__init__.py`.

--- triggered/__init__.py
+++ triggered/__init__.py
@@ -1,11 +1,12 @@
 import json
 from pathlib import Path
 
+from redbot.core.bot import Red
 from .triggered import Triggered
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-async def setup(bot):
+async def setup(bot: Red):
     await bot.add_cog(Triggered(bot))
