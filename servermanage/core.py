1e1e6b1a7452ae2a778d552b02ed50fbe9a7fd0b
quachtridat <quachtridat@yahoo.com.vn>
2023-02-12 14:01:43 -0800

    [ServerManage] Make logger use YYYY-MM-DD date fmt
    Address review comment
    https://github.com/SFUAnime/Ren/pull/620#discussion_r1103712067

--- servermanage/core.py
+++ servermanage/core.py
@@ -33,7 +33,7 @@ class Core:
             logPath = pathJoin(self.dataFolder, "info.log")
             handler = logging.FileHandler(filename=logPath, encoding="utf-8", mode="a")
             handler.setFormatter(
-                logging.Formatter("%(asctime)s %(message)s", datefmt="[%d/%m/%Y %H:%M:%S]")
+                logging.Formatter("%(asctime)s %(message)s", datefmt="[%Y/%m/%d %H:%M:%S]")
             )
             self.logger.addHandler(handler)
 
