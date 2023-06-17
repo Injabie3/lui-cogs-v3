36d640f457f3f993657434cd769f3c36a8fc13ed
Lui <injabie3@gmail.com>
2023-06-17 00:45:02 -0700

    [VxTwit] Extract enable to variable
    Co-authored-by: Dat Quach <quachtridat@yahoo.com.vn>

--- vxtwitterconverter/commandsCore.py
+++ vxtwitterconverter/commandsCore.py
@@ -6,8 +6,9 @@ from .constants import KEY_ENABLED
 
 class CommandsCore(Core):
     async def cmdToggle(self, ctx: Context):
-        enabled = not await self.config.guild(ctx.guild).get_attr(KEY_ENABLED)()
-        await self.config.guild(ctx.guild).get_attr(KEY_ENABLED).set(enabled)
+        enabledCfg = self.config.guild(ctx.guild).get_attr(KEY_ENABLED)
+        enabled = not await enabledCfg()
+        await enabledCfg.set(enabled)
 
         status = "enabled" if enabled else "disabled"
         await ctx.send(f"VxTwitter replacements are now {status}.")
